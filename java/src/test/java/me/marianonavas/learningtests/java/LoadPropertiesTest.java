3943c2d7b999ab8e6df58f41bcc4a13e30ce25b7
Mariano Navas <mariano.navas@taptapnetworks.com>
2013-09-24 10:31:12 +0200

    Recalling how to load properties from classpath resource

---
+++ java/src/test/java/me/marianonavas/learningtests/java/LoadPropertiesTest.java
@@ -0,0 +1,21 @@
+package me.marianonavas.learningtests.java;
+
+import static org.junit.Assert.*;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.Test;
+
+public class LoadPropertiesTest {
+    @Test
+    public void testLoadPropertiesFromClasspath() throws Exception {
+        String propsFile = "/myprops.properties";
+        InputStream resourceAsStream = getClass().getResourceAsStream(propsFile);
+        Properties props = new Properties();
+        props.load(resourceAsStream);
+        String actual = props.getProperty("prop1");
+        assertEquals("1", actual);
+        resourceAsStream.close();
+    }
+}
